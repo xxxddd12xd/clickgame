6d2fa8dc34a402a85d8c9948b832792a23c0c2f5
xxxddd12xd <xxxddd12xdd@yahoo.com.tw>
2021-08-16 04:45:44 +0800

    Initial

--- like_button.js
+++ like_button.js
@@ -11,28 +11,9 @@ function App() {
     </div>
   );
 }
-function App2() {
-  return (
-    <div>
-      <Welcome name="1" />
-      <Welcome name="2" />
-      <Welcome name="3" />
-    </div>
-  );
-}
-function App3() {
-  return (
-    <div>
-      <Welcome name="4" />
-      <Welcome name="5" />
-      <Welcome name="6" />
-    </div>
-  );
-}
+
 
 ReactDOM.render(
   <App />,
-  <App2 />,
-  <App3 />,
   document.getElementById('root')
 );
