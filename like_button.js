602486e9303e1db599361aa35743daab8b0bbdf1
xxxddd12xd <xxxddd12xdd@yahoo.com.tw>
2021-08-16 03:37:30 +0800

    Initial

--- like_button.js
+++ like_button.js
@@ -1,25 +1,4 @@
-'use strict';
-
-const e = React.createElement;
-
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
-}
-// ... 你貼上的 starter code ...
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
