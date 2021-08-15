99d02b67f23e1e740063c537e30aecafb930043c
xxxddd12xd <xxxddd12xdd@yahoo.com.tw>
2021-08-16 04:40:04 +0800

    Initial

--- like_button.js
+++ like_button.js
@@ -1,21 +1,14 @@
-
-function clickArea(){
-  return <div>1</div>
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
 }
 
-function clickArea2(){
-  return <div>2</div>
-}
 
-function clickArea3(){
-  return <div>3</div>
-}
 
-const element1 = <clickArea />
-const element2 = <clickArea2 />
-const element3 = <clickArea3 />
+const element = <Welcome name="1" />;
+const element2 = <Welcome name="2" />;
+const element3 = <Welcome name="3" />;
 ReactDOM.render(
-  element1,
+  element,
   element2,
   element3,
   document.getElementById('root')
