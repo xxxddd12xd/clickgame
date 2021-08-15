c3fe9526046ca91faf068679afdf06abb9e9576f
xxxddd12xd <xxxddd12xdd@yahoo.com.tw>
2021-08-16 04:35:23 +0800

    Initial

--- like_button.js
+++ like_button.js
@@ -1,9 +1,22 @@
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+
+function clickArea(){
+  return <div>1</div>
 }
 
-const element = <Welcome name="Sara" />;
+function clickArea2(){
+  return <div>2</div>
+}
+
+function clickArea3(){
+  return <div>3</div>
+}
+
+const clickArea = <clickArea />
+const clickArea2 = <clickArea2 />
+const clickArea3 = <clickArea3 />
 ReactDOM.render(
-  element,
+  clickArea,
+  clickArea2,
+  clickArea3,
   document.getElementById('root')
 );
