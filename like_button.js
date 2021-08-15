0f7f859298e20ab60c4e46351d632188aa3ea77a
xxxddd12xd <xxxddd12xdd@yahoo.com.tw>
2021-08-16 04:43:11 +0800

    Initial

--- like_button.js
+++ like_button.js
@@ -2,9 +2,17 @@ function Welcome(props) {
   return <h1>Hello, {props.name}</h1>;
 }
 
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
 
-const element = <Welcome name="Sara" />;
 ReactDOM.render(
-  element,
+  <App />,
   document.getElementById('root')
 );
