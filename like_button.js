f53e851d38266d6550e8c37f14b16ad6dfaddc79
xxxddd12xd <xxxddd12xdd@yahoo.com.tw>
2021-08-16 04:25:50 +0800

    Initial

--- like_button.js
+++ like_button.js
@@ -1,7 +1,7 @@
-import clickArea from './components/clickArea'
+import clickArea from './components/clickArea.js'
 
 ReactDOM.render(
-  <clickArea />,
+  clickArea,
   <h1>Jeff217025818</h1>,
   document.getElementById('root')
 );
