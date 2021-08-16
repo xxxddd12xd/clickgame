5ce83231441e540a785b740d3885e8687e1b9fd8
xxxddd12xd <xxxddd12xdd@yahoo.com.tw>
2021-08-16 12:43:47 +0800

    Initial

--- like_button.js
+++ like_button.js
@@ -1,6 +1,16 @@
 function Welcome(props) {
-  return <div ><h1>Hello, {props.name}</h1></div>;
+  return <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">{props.name}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+    </div>
+  </div>;
 }
+
+
 
 function App() {
   return (
