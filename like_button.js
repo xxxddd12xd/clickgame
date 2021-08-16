15472e9d074fd92d9340f20bfe24af68205b7d6f
xxxddd12xd <xxxddd12xdd@yahoo.com.tw>
2021-08-16 12:52:47 +0800

    Initial

--- like_button.js
+++ like_button.js
@@ -1,13 +1,15 @@
 function Welcome(props) {
-  return <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">{props.name}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-    </div>
-  </div>;
+  return <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>{props.name}</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
 }
 
 
