f5261ddaafbd30e7b7c3aca6b61b4f65032be93f
xxxddd12xd <xxxddd12xdd@yahoo.com.tw>
2021-08-16 17:39:47 +0800

    Initial

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import MyCard from './components/ClickArea';
 
+function App() {
+  return (
+    <div>
+  <MyCard title="肉便區"/>
+  <MyCard title="點擊區"/>
+  <MyCard title="超級肉便區"/>
+    </div>
+  );
+}
 
 
 
 ReactDOM.render(
-  <MyCard />,
+  <App />,
   document.getElementById('root')
 );
