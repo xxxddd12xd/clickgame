e18b8527dc4733d61ea4a24220d25e979dedf90a
xxxddd12xd <xxxddd12xdd@yahoo.com.tw>
2021-08-16 12:59:30 +0800

    Initial

--- like_button.js
+++ like_button.js
@@ -1,3 +1,5 @@
+<script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js" crossorigin></script>
+
 function Welcome(props) {
   return <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src="holder.js/100px180" />
