7a4668056b1c85afba006627956600a8f9227266
xxxddd12xd <xxxddd12xdd@yahoo.com.tw>
2021-08-31 03:47:42 +0800

    Initial

--- src/index.js
+++ src/index.js
@@ -21,8 +21,24 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-var clickAmount=0
 
+
+//test
+
+//test
+try{
+  var clickAmount=JSON.parse(document.cookie)[0].clickAmount
+}
+catch(e)
+{
+  var clickAmount=0
+}
 document.querySelectorAll(".card")[1].lastChild.lastChild.addEventListener("click",function(){
+  clickAmount=parseInt(clickAmount,10)
   clickAmount+=1
-  document.querySelectorAll(".card")[1].lastChild.childNodes[1].innerHTML = "你已經屁了"+clickAmount+"次";})
+  document.querySelectorAll(".card")[1].lastChild.childNodes[1].innerHTML = "你已經屁了"+clickAmount+"次";
+  var info=[{clickAmount:clickAmount,money:'999'}]
+  var info_json=JSON.stringify(info)
+  document.cookie=info_json+"; expires=Thu, 18 Dec 2022 12:00:00 GMT; path=/";
+})
+
