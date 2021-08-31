7a8d2cc5c0269eae20d070b184be80f249d810af
xxxddd12xd <xxxddd12xdd@yahoo.com.tw>
2021-08-31 22:45:26 +0800

    Initial

--- src/components/ClickArea.js
+++ src/components/ClickArea.js
@@ -1,6 +1,7 @@
 import {Card,Button} from 'react-bootstrap'
 
-function MyCard(props) {
+
+function Left(props) {
   return(
     <div className={"h-100 d-inline-block"} style={{width: '33%'}}>
       <Card style={{ width: '100%' }}>
@@ -8,8 +9,7 @@ function MyCard(props) {
           <Card.Body>
           <Card.Title>{props.title}</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
+            還沒想到
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
         </Card.Body>
@@ -19,4 +19,50 @@ function MyCard(props) {
   )
   }
 
-export default MyCard
+function MyCard(props) {
+  return(
+    <div className={"h-100 d-inline-block"} style={{width: '33%'}}>
+      <Card style={{ width: '100%' }}>
+        <Card.Img variant="top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17b4e20590a%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17b4e20590a%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22105.9765625%22%20y%3D%2297.003125%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" />
+          <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text>
+            你已經屁了{props.text}次,每秒自動屁{props.auto}次
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
+
+  )
+  }
+
+  function Upgrades(props) {
+    return(
+      <div className={"h-100 d-inline-block"} style={{width: '33%'}}>
+        <Card style={{ width: '100%' }}>
+          <Card.Img variant="top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17b4e20590a%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17b4e20590a%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22105.9765625%22%20y%3D%2297.003125%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" />
+            <Card.Body>
+              <Card.Title>{props.title}</Card.Title>
+              <Card.Text>
+                一些升級資訊
+              </Card.Text>
+              <Card style={{ backgroundColor:'gray' }}>
+                <div className="container"style={{ height: '30px' }}>
+                  <div className="row">
+                    <div  className="col-sm">每秒多屁一次</div>
+                      <div  className="col-sm-2">
+                      <Button type="button"className="col p-1 btn-sm "variant="primary">Buy</Button>
+                    </div>
+                  </div>
+                </div>
+
+              </Card>
+            </Card.Body>
+        </Card>
+      </div>
+  
+    )
+    }
+
+export {Left,MyCard,Upgrades}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import MyCard from './components/ClickArea';
+import {Left,MyCard,Upgrades} from './components/ClickArea';
+
+try{
+  var clickAmount=JSON.parse(document.cookie)[0].clickAmount
+  var autoClick=JSON.parse(document.cookie)[0].autoClick
+}
+catch(e)
+{
+  var clickAmount=0
+  var autoClick=0
+}
 
 
 function App() {
   return (
     <div>
-  <MyCard title="左邊"/>
-  <MyCard title="點擊區"/>
-  <MyCard title="右邊"/>
+  <Left title="左邊"/>
+  <MyCard title="點擊區" text={clickAmount}auto={autoClick}/>
+  <Upgrades title="右邊"/>
     </div>
   );
 }
@@ -22,23 +32,31 @@ ReactDOM.render(
 );
 
 
-
-//test
-
-//test
-try{
-  var clickAmount=JSON.parse(document.cookie)[0].clickAmount
-}
-catch(e)
-{
-  var clickAmount=0
-}
+//點擊
 document.querySelectorAll(".card")[1].lastChild.lastChild.addEventListener("click",function(){
   clickAmount=parseInt(clickAmount,10)
   clickAmount+=1
-  document.querySelectorAll(".card")[1].lastChild.childNodes[1].innerHTML = "你已經屁了"+clickAmount+"次";
-  var info=[{clickAmount:clickAmount,money:'999'}]
-  var info_json=JSON.stringify(info)
-  document.cookie=info_json+"; expires=Thu, 18 Dec 2022 12:00:00 GMT; path=/";
+  document.querySelectorAll(".card")[1].lastChild.childNodes[1].innerHTML = "你已經屁了"+clickAmount+"次"+",每秒自動屁"+autoClick+"次";
+})
+//自動屁升級
+document.querySelectorAll(".card")[2].lastChild.lastChild.lastChild.lastChild.lastChild.lastChild.addEventListener("click",function(){
+  autoClick+=1
+  document.querySelectorAll(".card")[1].lastChild.childNodes[1].innerHTML = "你已經屁了"+clickAmount+"次"+",每秒自動屁"+autoClick+"次";
+  // console.log(autoClick)
 })
 
+//自動屁
+var clicksPerSecond = setInterval(myAlert, 1000);
+function myAlert() {
+  // console.log('1秒鐘到了！,每秒+'+autoClick);
+  clickAmount+=autoClick
+  document.querySelectorAll(".card")[1].lastChild.childNodes[1].innerHTML = "你已經屁了"+clickAmount+"次"+",每秒自動屁"+autoClick+"次";
+}
+//存檔(每一分鐘)
+var saveTimer= setInterval(save,60000)
+function save(){
+  var info=[{clickAmount:clickAmount,autoClick:autoClick}]
+  var info_json=JSON.stringify(info)
+  document.cookie=info_json+"; expires=Thu, 18 Dec 2022 12:00:00 GMT; path=/";
+  console.log("saved!")
+}
