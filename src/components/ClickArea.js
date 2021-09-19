4f1f1fb4fbd2c5462fc34d1ba48e93d6de29f0c5
xxxddd12xd <xxxddd12xdd@yahoo.com.tw>
2021-09-19 22:42:55 +0800

    Initial

--- src/components/ClickArea.js
+++ src/components/ClickArea.js
@@ -10,13 +10,15 @@ import Upgrade2 from'./Upgrade2.png'
 import Upgrade3 from'./Upgrade3.png'
 import Upgrade4 from'./Upgrade4.png'
 import Upgrade5 from'./Upgrade5.png'
-
+import Bonk1 from'./bonk1.png'
+import Bonk2 from'./bonk2.png'
+import BonkSoundEffect from './Bonk.mp3'
 
 
 function Left(props) {
   return(
     <div className={"h-100 d-inline-block"} style={{width: '33%'}}>
-      <Card style={{ width: '100%' }}>
+      <Card style={{ width: '100%',backgroundColor:'rgb(115,92,64)'}}>
         <Card.Img src={ItemShop}/>
         <Row>
           <Col><img src={Apple}/></Col>
@@ -44,15 +46,15 @@ function Left(props) {
 
 function MyCard(props) {
   return(
-    <div className={"h-100 d-inline-block"} style={{width: '33%'}}>
-      <Card style={{ width: '100%' }}>
-      <iframe height="265" src="https://www.youtube.com/embed/5qap5aO4i9A?autoplay=1&controls=1&modestbranding=1&showinfo=0&enablejsapi=1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen ></iframe>
+    <div className={"h-100 d-inline-block text-light"} style={{width: '33%'}}>
+      <Card style={{ width: '100%' ,backgroundColor:'rgb(18,53,79)'}}>
+          <Card.Img src={Bonk1}/>
           <Card.Body>
           <Card.Title>{props.title}</Card.Title>
           <Card.Text>
             你已經被屁了{props.text}次,每秒自動屁你{props.auto}次
           </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">屁一下</Button>
         </Card.Body>
       </Card>
     </div>
@@ -76,7 +78,7 @@ class Upgrade_detail extends React.Component{
       <Card className="upgrade_detail d-none"style={{ backgroundColor:'gray' }}>
         <div className="container"style={{ height: '30px' }}>
           <div className="row">
-            <div  className="col-sm"><img src={this.props.img} style={{ height: '32px', width: '32px' }}></img></div>
+            <div  className="col-sm ownedUpgrade"><img src={this.props.img} style={{ height: '32px', width: '32px' }}></img>{this.props.ownedUpgrade}</div>
             <div  className="col-sm">{this.props.test.name}</div>
             <div  className="col-sm cost">{this.props.cost}ϝ</div>
             <div  className="col-sm-2">
@@ -95,21 +97,21 @@ class Upgrade_detail extends React.Component{
 class Upgrades extends React.Component{
   render(){
     return(
-      <div className={"h-100 d-inline-block"} style={{width: '33%'}}>
-      <Card style={{ width: '100%' }}>
+      <div className={"h-100 d-inline-block text-light"} style={{width: '33%'}}>
+      <Card style={{ width: '100%' ,backgroundColor:'rgb(18,53,79)'}}>
         <Card.Img variant="top" src={Shop}/>
           <Card.Body>
             <Card.Title>{this.props.title}</Card.Title>
-            <Card.Text>
+            <Card.Text class="text-light">
               自己屁自己不夠，需要打手?
             </Card.Text>
             <Container className="upgradeContainer">
-              <Upgrade_detail img={Upgrade0}test={Upgrade_detail_context[0]}order={this.props.autoPerSec[0]} cost={this.props.cost[0]}/>
-              <Upgrade_detail img={Upgrade1}test={Upgrade_detail_context[1]}order={this.props.autoPerSec[1]} cost={this.props.cost[1]}/>
-              <Upgrade_detail img={Upgrade2}test={Upgrade_detail_context[2]}order={this.props.autoPerSec[2]} cost={this.props.cost[2]}/>
-              <Upgrade_detail img={Upgrade3}test={Upgrade_detail_context[3]}order={this.props.autoPerSec[3]} cost={this.props.cost[3]}/>
-              <Upgrade_detail img={Upgrade4}test={Upgrade_detail_context[4]}order={this.props.autoPerSec[4]} cost={this.props.cost[4]}/>
-              <Upgrade_detail img={Upgrade5}test={Upgrade_detail_context[5]}order={this.props.autoPerSec[5]} cost={this.props.cost[5]}/>
+              <Upgrade_detail img={Upgrade0}test={Upgrade_detail_context[0]}order={this.props.autoPerSec[0]} cost={this.props.cost[0]}ownedUpgrade={this.props.ownedUpgrade[0]}/>
+              <Upgrade_detail img={Upgrade1}test={Upgrade_detail_context[1]}order={this.props.autoPerSec[1]} cost={this.props.cost[1]}ownedUpgrade={this.props.ownedUpgrade[1]}/>
+              <Upgrade_detail img={Upgrade2}test={Upgrade_detail_context[2]}order={this.props.autoPerSec[2]} cost={this.props.cost[2]}ownedUpgrade={this.props.ownedUpgrade[2]}/>
+              <Upgrade_detail img={Upgrade3}test={Upgrade_detail_context[3]}order={this.props.autoPerSec[3]} cost={this.props.cost[3]}ownedUpgrade={this.props.ownedUpgrade[3]}/>
+              <Upgrade_detail img={Upgrade4}test={Upgrade_detail_context[4]}order={this.props.autoPerSec[4]} cost={this.props.cost[4]}ownedUpgrade={this.props.ownedUpgrade[4]}/>
+              <Upgrade_detail img={Upgrade5}test={Upgrade_detail_context[5]}order={this.props.autoPerSec[5]} cost={this.props.cost[5]}ownedUpgrade={this.props.ownedUpgrade[5]}/>
             </Container>
           </Card.Body>
       </Card>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {Left,MyCard,Upgrades,Footer} from './components/ClickArea';
+import BonkSoundEffect from './Bonk.mp3'
+
+
 
 try{
   var clickAmount=JSON.parse(document.cookie)[0].clickAmount
@@ -9,6 +12,7 @@ try{
   var upgrade_status=JSON.parse(document.cookie)[0].upgrade_status
   var costList=JSON.parse(document.cookie)[0].costList
   var autoPerSec=[1,2,8,47,260,1400,7800]
+  var ownedUpgrade=JSON.parse(document.cookie)[0].ownedUpgrade
 }
 catch(e)
 {
@@ -17,15 +21,17 @@ catch(e)
   upgrade_status=0
   costList=[15,100,1100,12000,130000,1400000]
   autoPerSec=[1,2,8,47,260,1400,7800]
+  ownedUpgrade=[0,0,0,0,0,0]
 }
 
 class App extends React.Component{
   render(){
     return(
       <div>
+      <audio id="sound1" src={BonkSoundEffect} preload="auto"></audio>
       <Left title="左邊"/>
       <MyCard title="點擊區" text={clickAmount} auto={autoClick}/>
-      <Upgrades title="升級" autoPerSec={autoPerSec} cost={costList}/>
+      <Upgrades title="升級" autoPerSec={autoPerSec} cost={costList}ownedUpgrade={ownedUpgrade}/>
       <Footer></Footer>
       </div>
     )
@@ -75,6 +81,9 @@ function ifCanUpgrade(){
 document.querySelectorAll(".card")[1].lastChild.lastChild.addEventListener("click",function(){
   clickAmount=parseInt(clickAmount,10)
   clickAmount+=1
+  document.getElementById('sound1').pause();
+  document.getElementById('sound1').currentTime = 0;
+  document.getElementById('sound1').play()
   document.querySelectorAll(".card")[1].lastChild.childNodes[1].innerHTML = "你已經被屁了"+clickAmount+"次"+",每秒自動屁"+autoClick+"次";
   ifCanUpgrade()
 })
@@ -83,8 +92,10 @@ document.querySelectorAll(".card")[1].lastChild.lastChild.addEventListener("clic
 document.querySelectorAll(".upgradeButton")[0].addEventListener("click",function(){
   autoClick+=1
   upgrade_status=1
+  ownedUpgrade[0]+=1
   clickAmount-=costList[0]
   costList[0]=Math.ceil(costList[0]*1.15)
+  document.querySelectorAll(".ownedUpgrade")[0].childNodes[1].textContent=ownedUpgrade[0]
   document.querySelectorAll(".cost")[0].innerText=costList[0]+'ϝ'
   document.querySelectorAll(".card")[1].lastChild.childNodes[1].innerHTML = "你已經被屁了"+clickAmount+"次"+",每秒自動屁"+autoClick+"次";
   ifCanUpgrade()
@@ -95,8 +106,10 @@ document.querySelectorAll(".upgradeButton")[0].addEventListener("click",function
 document.querySelectorAll(".upgradeButton")[1].addEventListener("click",function(){
   autoClick+=2
   upgrade_status=2
+  ownedUpgrade[1]+=1
   clickAmount-=costList[1]
   costList[1]=Math.ceil(costList[1]*1.15)
+  document.querySelectorAll(".ownedUpgrade")[1].childNodes[1].textContent=ownedUpgrade[1]
   document.querySelectorAll(".cost")[1].innerText=costList[1]+'ϝ'
   document.querySelectorAll(".card")[1].lastChild.childNodes[1].innerHTML = "你已經被屁了"+clickAmount+"次"+",每秒自動屁"+autoClick+"次";
   ifCanUpgrade()
@@ -107,8 +120,10 @@ document.querySelectorAll(".upgradeButton")[1].addEventListener("click",function
 document.querySelectorAll(".upgradeButton")[2].addEventListener("click",function(){
   autoClick+=8
   upgrade_status=3
+  ownedUpgrade[2]+=1
   clickAmount-=costList[2]
   costList[2]=Math.ceil(costList[2]*1.15)
+  document.querySelectorAll(".ownedUpgrade")[2].childNodes[1].textContent=ownedUpgrade[2]
   document.querySelectorAll(".cost")[2].innerText=costList[2]+'ϝ'
   document.querySelectorAll(".card")[1].lastChild.childNodes[1].innerHTML = "你已經被屁了"+clickAmount+"次"+",每秒自動屁"+autoClick+"次";
   ifCanUpgrade()
@@ -119,8 +134,10 @@ document.querySelectorAll(".upgradeButton")[2].addEventListener("click",function
 document.querySelectorAll(".upgradeButton")[3].addEventListener("click",function(){
   autoClick+=47
   upgrade_status=4
+  ownedUpgrade[3]+=1
   clickAmount-=costList[3]
   costList[3]=Math.ceil(costList[3]*1.15)
+  document.querySelectorAll(".ownedUpgrade")[3].childNodes[1].textContent=ownedUpgrade[3]
   document.querySelectorAll(".cost")[3].innerText=costList[3]+'ϝ'
   document.querySelectorAll(".card")[1].lastChild.childNodes[1].innerHTML = "你已經被屁了"+clickAmount+"次"+",每秒自動屁"+autoClick+"次";
   ifCanUpgrade()
@@ -131,8 +148,10 @@ document.querySelectorAll(".upgradeButton")[3].addEventListener("click",function
 document.querySelectorAll(".upgradeButton")[4].addEventListener("click",function(){
   autoClick+=260
   upgrade_status=5
+  ownedUpgrade[4]+=1
   clickAmount-=costList[4]
   costList[4]=Math.ceil(costList[4]*1.15)
+  document.querySelectorAll(".ownedUpgrade")[4].childNodes[1].textContent=ownedUpgrade[4]
   document.querySelectorAll(".cost")[4].innerText=costList[4]+'ϝ'
   document.querySelectorAll(".card")[1].lastChild.childNodes[1].innerHTML = "你已經被屁了"+clickAmount+"次"+",每秒自動屁"+autoClick+"次";
   ifCanUpgrade()
@@ -151,7 +170,7 @@ function myAlert() {
 //存檔(每一分鐘)
 var saveTimer= setInterval(save,6000)
 function save(){
-  var info=[{clickAmount:clickAmount,autoClick:autoClick,upgrade_status:upgrade_status,costList:costList}]
+  var info=[{clickAmount:clickAmount,autoClick:autoClick,upgrade_status:upgrade_status,costList:costList,ownedUpgrade:ownedUpgrade}]
   var info_json=JSON.stringify(info)
   document.cookie=info_json+"; expires=Thu, 18 Dec 2022 12:00:00 GMT; path=/";
   console.log("saved!")
